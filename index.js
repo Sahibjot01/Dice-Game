fabac0703a196e9026436ce8574b0292a3878b0a
Sahibjot01 <sahibjot70@gmail.com>
2020-05-12 01:03:23 +0530

    final website

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var randomNumber1 = Math.random();
-var randomNumber2 = Math.random();
-randomNumber1 *= 6;
-randomNumber2 *= 6;
-randomNumber1 = Math.ceil(randomNumber1);
-randomNumber2 = Math.ceil(randomNumber2);
+function diceGame(){
+  var randomNumber1 = Math.random();
+  var randomNumber2 = Math.random();
+  randomNumber1 *= 6;
+  randomNumber2 *= 6;
+  randomNumber1 = Math.ceil(randomNumber1);
+  randomNumber2 = Math.ceil(randomNumber2);
 
-document.querySelectorAll("img")[0].setAttribute("src","images/dice"+randomNumber1+".png");
-document.querySelectorAll("img")[1].setAttribute("src","images/dice"+randomNumber2+".png");
+  document.querySelectorAll("img")[0].setAttribute("src","images/dice"+randomNumber1+".png");
+  document.querySelectorAll("img")[1].setAttribute("src","images/dice"+randomNumber2+".png");
 
-if(randomNumber1>randomNumber2){document.querySelector("h1").innerHTML = "<i class='far fa-flag'></i> Player 1 wins!";}
-else if(randomNumber1<randomNumber2){document.querySelector("h1").innerHTML = "Player 2 wins! <i class='far fa-flag'></i>";}
-else{document.querySelector("h1").innerHTML = "Draw!";}
+  if(randomNumber1>randomNumber2){document.querySelector("h2").innerHTML = "Player 1 wins!";}
+  else if(randomNumber1<randomNumber2){document.querySelector("h2").innerHTML = "Player 2 wins!";}
+  else{document.querySelector("h2").innerHTML = "Draw!";}
+}
